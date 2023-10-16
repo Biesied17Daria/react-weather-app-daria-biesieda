6bf02633bf6e6fafc31ebcfa79e8dce7c0d6da92
Daria <rafflezya.kaleana@gmail.com>
2023-10-16 15:17:35 +0200

    aded url site portfolio Daria Biesieda

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         </h1>
     
     <footer>
-      This project was coded by <a href='' target='_blank'>Daria Biesieda</a>  and is {" "}
+      This project was coded by <a href='https://deft-narwhal-77936b.netlify.app/' target='_blank'>Daria Biesieda</a>  and is {" "}
     <a href='https://github.com/Biesied17Daria/react-weather-app-daria-biesieda'
     target='_blank' >
       open-sourced on GitHub

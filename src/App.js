87ef176b6ef0d3da1015b1ab006b901a8dd88f18
Daria <rafflezya.kaleana@gmail.com>
2023-10-16 12:15:23 +0200

    Aded a GitHub rep URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Wearher App
+      <h1>
+        Wearher App
+        </h1>
+    
+    <footer>
+      This project was coded by <a href='' target='_blank'>Daria Biesieda</a>  and is {" "}
+    <a href='https://github.com/Biesied17Daria/react-weather-app-daria-biesieda'
+    target='_blank' >
+      open-sourced on GitHub
+      </a>
+    </footer>
     </div>
   );
 }

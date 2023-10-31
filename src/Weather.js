d42b93f11c93ca9bd62829b0915d317cc692c376
Daria <rafflezya.kaleana@gmail.com>
2023-10-31 08:25:58 +0100

    aded Search engineer

--- src/Weather.js
+++ src/Weather.js
@@ -1,83 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import FormattedDate from './FormattedDate';
-import './Weather.css';
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import "./Weather.css";
 
 export default function Weather(props) {
- 
-  const [weatherData, setWeatherData] = useState({ready: false});
-  const [city, setCity] = useState('Lisbon');
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  useEffect(() => {
-    const apiKey = 'b1413ed7e08bdb32f52c4b761d92a978';
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-      
-    axios.get(apiUrl).then((response) => {
-      setWeatherData ({
-        ready: true,
-        temperature: response.data.main.temp,
-        description: response.data.weather[0].description,
-        date: new Date(response.data.dt * 1000),
-        iconUrl: 'https://img.freepik.com/free-vector/hand-drawn-chuva-de-amor-decoration-element-collection_23-2148919613.jpg?size=626&ext=jpg',
-        humidity: response.data.main.humidity,
-        wind: response.data.wind.speed,
-        city: response.data.name
-    })
-
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
+  }
 
-  }, [city]);
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "625a9fb278bb149ed779cbab420da9ef";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   if (weatherData.ready) {
     return (
-      <div className='Weather'>
-        { <><form>
-                <div className='row'>
-                    <div className='col-9'>
-                        <input
-                            type='search'
-                            placeholder='Search your city...'
-                            className='form-control'
-                            autoFocus='on' />
-                    </div>
-                    <div className='col-3'>
-                        <input
-                            type='submit'
-                            value='Search'
-                            className='btn btn-primary w-100' />
-                    </div>
-                </div>
-            </form><h1>
-                    {weatherData.city}
-                </h1><ul>
-                    <li>
-                    <FormattedDate date={weatherData.date} />
-                    </li>
-                    <li className='text-capitalize'>
-                        {weatherData.description}
-                    </li>
-                </ul><div className='row mt-3'>
-                    <div className='col-6'>
-                        <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description} />
-                        <span className='temperature'>{Math.round(weatherData.temperature)}</span>
-                        <span className='unit'>°С</span>
-                    </div>
-                    <div className='col-6'>
-                        <ul>
-                            <li>
-                                Humidity: {weatherData.humidity}%
-                            </li>
-                            <li>
-                                Wind: {weatherData.wind} km/h
-                            </li>
-                        </ul>
-                    </div>
-                </div></>}
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
+    search();
     return "Loading...";
   }
 }

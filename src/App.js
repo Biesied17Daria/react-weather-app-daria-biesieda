91025800a3346afcf3c1306ccd5c37702aed42e0
Daria <rafflezya.kaleana@gmail.com>
2023-11-03 23:13:41 +0100

    aded atribute

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className='container'>
         <Weather defaultCity='Berlin'/>
     <footer>
-      This project was coded by <a href='https://deft-narwhal-77936b.netlify.app/' target='_blank'>Daria Biesieda</a>  and is {" "}
+      This project was coded by <a href='https://deft-narwhal-77936b.netlify.app/' target='_blank' rel='noreferrer'>Daria Biesieda</a>  and is {" "}
     <a href='https://github.com/Biesied17Daria/react-weather-app-daria-biesieda'
     target='_blank' rel='noreferrer'>
       open-sourced on GitHub
